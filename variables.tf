d0637fedc2c3738167d0a3a3a308f558205891a1
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-04 03:50:37 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,5 @@
 # vCloud Director Organization Variables
 
-variable "vcenter_name" {
-    type        = string
-    description = "vCenter Name"
-}
-
 variable "cd_username" {
     type        = string
     description = "CloudDirector UserName"
@@ -18,3 +13,63 @@ variable "cd_vdcadres" {
     type        = string
     description = "CloudDirector Adresi"
 }
+
+variable "vcenter_name" {
+    type        = string
+    description = "vCenter Name"
+}
+
+variable "org_name" {
+    type        = string
+    description = "Organizasyon İsmi"
+}
+
+
+variable "vdc_name" {
+    type        = string
+    description = "VDC Name"
+
+}
+
+variable "dvport_name" {
+    type        = string
+    description = "vCenter VLAN Adı"
+    
+}
+variable "extnet_name" {
+    type        = string
+    description = "External Network Name"
+}
+variable "extnet_description" {
+    type        = string
+    description = "External Network Description"
+}
+variable "extnet_gw" {
+    type        = string
+    description = "External Network Gateway"
+}
+variable "extnet_mask" {
+    type        = string
+    description = "External Network Mask"
+}
+variable "extnet_dns1" {
+    type        = string
+    description = "External Network DNS1"
+}
+variable "extnet_dns2" {
+    type        = string
+    description = "External Network DNS2"
+}
+variable "extnet_suffix" {
+    type        = string
+    description = "External Network DNS Suffix"
+}
+variable "extnet_ip_pool_start" {
+    type        = string
+    description = "External Network IP Pool Start Address"
+}
+variable "extnet_ip_pool_end" {
+    type        = string
+    description = "External Network IP Pool End Address"
+}
+
